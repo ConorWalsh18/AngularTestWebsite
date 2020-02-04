824d4f71826a36416bdf0b60499368300ffa9de4
ConorWalsh18 <conor.walsh@colorado.edu>
2020-02-04 16:40:26 -0700

    Updated the traversal page

--- src/app/test-page-two/test-page-two.component.ts
+++ src/app/test-page-two/test-page-two.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  group
+} from '@angular/animations';
 
 @Component({
   selector: 'test-page-two',
   templateUrl: './test-page-two.component.html',
-  styleUrls: ['./test-page-two.component.css']
+  styleUrls: ['./test-page-two.component.css'],
+  animations: [ 
+    trigger('moveArrow', [
+      state('goDown', style({ top: '144%' })),
+      state('goUp', style({ top: '51%' })),
+      //state('hideGoDown', style({ top: '144%', opacity: 0})),
+      transition('* <=> *', [animate('0.6s ease-in-out')])
+    ]),
+    trigger('moveArrowBody', [
+      state('goDown', style({ height: '1080px', transform: 'translate(-50%, 0%)' })),
+      state('goUp', style({ height: '1100px', top: '98%' })),
+      //state('hideGoDown', style({ height: '1080px', transform: 'translate(-50%, 0%)', opacity: 0 })),
+      transition('* <=> *', [animate('0.6s ease-in-out')])
+    ])
+  ]
 })
 export class TestPageTwoComponent implements OnInit {
+
+  moveArrow: string = "hide";
 
   constructor() { }
 
@@ -14,12 +39,18 @@ export class TestPageTwoComponent implements OnInit {
 
   traversePage(direction: string) {
     if (direction === "down") {
+      this.moveArrow = "goDown";
       window.scroll({
         top: window.innerHeight,
         behavior: 'smooth'
       });
+
+      //setTimeout(() => {
+      //  this.moveArrow = "hideGoDown";
+      //}, 600);
     }
     else {
+      this.moveArrow = "goUp";      
       window.scroll({
         top: 0,
         behavior: 'smooth'
